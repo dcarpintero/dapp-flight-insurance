99425e0d93c6eb4dc10d6b10057423e695b08edf
dcarpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-14 18:40:49 +0200

    Upgrade to Solidity 0.6.0

--- truffle-config.js
+++ truffle-config.js
@@ -11,9 +11,24 @@ module.exports = {
       network_id: "*",
     },
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.4.24",
+      version: "0.6.0", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
     },
   },
 };
