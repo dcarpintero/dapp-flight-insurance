927ab50d87d71051e6ae3206c7f750fb86c88ac4
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-24 23:40:18 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     development: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50, false);
       },
       network_id: "*",
     },
