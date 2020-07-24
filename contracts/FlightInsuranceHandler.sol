bf52754e75cf4fdba9cd08347ae6eb4cb6cee4fa
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-24 19:20:11 +0200

    Update FlightInsuranceHandler.sol

--- contracts/FlightInsuranceHandler.sol
+++ contracts/FlightInsuranceHandler.sol
@@ -13,7 +13,13 @@ contract FlightInsuranceHandler is Ownable, AccessControl, PullPayment {
     using SafeMath for uint256;
     ConsortiumAlliance consortium;
 
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    /*
     constructor(address _consortiumAlliance) public {
         consortium = ConsortiumAlliance(_consortiumAlliance);
+    }*/
+    constructor() public {
+        _setupRole(ADMIN_ROLE, msg.sender);
     }
 }
