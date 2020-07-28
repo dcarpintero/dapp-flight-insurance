c9d47bc25801a2a266d79ba13abe1729e24753df
dcarpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-28 19:02:37 +0200

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,30 @@
-//const FlightSuretyApp = artifacts.require("FlightSuretyApp");
-//const FlightSuretyData = artifacts.require("FlightSuretyData");
+const ConsortiumSettings = artifacts.require("ConsortiumSettings");
 const ConsortiumAlliance = artifacts.require("ConsortiumAlliance");
 const FlightInsuranceHandler = artifacts.require("FlightInsuranceHandler");
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
+  deployer.then(async () => {
+    await deployer.deploy(ConsortiumSettings);
+    await deployer.deploy(ConsortiumAlliance, ConsortiumSettings.address);
+    await deployer.deploy(FlightInsuranceHandler, ConsortiumAlliance.address);
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-
-    deployer.deploy(ConsortiumAlliance)
-    .then(() => {
-        return deployer.deploy(FlightInsuranceHandler)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: ConsortiumAlliance.address,
-                            appAddress: FlightInsuranceHandler.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
-}
+    let config = {
+      localhost: {
+        url: "http://localhost:8545",
+        dataAddress: ConsortiumAlliance.address,
+        appAddress: FlightInsuranceHandler.address,
+      },
+    };
+    fs.writeFileSync(
+      __dirname + "/../src/dapp/config.json",
+      JSON.stringify(config, null, "\t"),
+      "utf-8"
+    );
+    fs.writeFileSync(
+      __dirname + "/../src/server/config.json",
+      JSON.stringify(config, null, "\t"),
+      "utf-8"
+    );
+  });
+};
