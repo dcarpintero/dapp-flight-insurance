5c4da194cbb69e685b7c2479b80f58369f9ad22b
dcarpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-31 19:31:46 +0200

    Update TestOracle.js

--- test/TestOracle.js
+++ test/TestOracle.js
@@ -1,37 +1,36 @@
-const ConsortiumAlliance = artifacts.require('ConsortiumAlliance')
-const ConsortiumOracle = artifacts.require('ConsortiumOracle')
-const FlightInsuranceHandler = artifacts.require('FlightInsuranceHandler')
-const assert = require('chai').assert
-const truffleAssert = require('truffle-assertions')
+const ConsortiumAlliance = artifacts.require("ConsortiumAlliance");
+const ConsortiumOracle = artifacts.require("ConsortiumOracle");
+const FlightInsuranceHandler = artifacts.require("FlightInsuranceHandler");
+const assert = require("chai").assert;
+const truffleAssert = require("truffle-assertions");
 
-contract('Oracle', async (accounts) => {
-  const OnlyValidKey = 'Invalid insurance key'
-  const ORACLE_FEE = web3.utils.toWei('1', 'ether')
+contract("Oracle", async (accounts) => {
+  const ORACLE_FEE = web3.utils.toWei("1", "ether");
 
-  before('setup contract', async () => {
-    insuranceHandler = await FlightInsuranceHandler.deployed()
-    oracle = await ConsortiumOracle.deployed()
-  })
+  before("setup contract", async () => {
+    insuranceHandler = await FlightInsuranceHandler.deployed();
+    oracle = await ConsortiumOracle.deployed();
+  });
 
-  describe('Registration and Responses', function () {
+  describe("Registration and Responses", function () {
     it(`lets register 20+ oracles`, async () => {
-      let fee = ORACLE_FEE
+      let fee = ORACLE_FEE;
 
-      let tx = await consortium.registerOracle({
+      let tx = await oracle.registerOracle({
         from: accounts[10],
         value: fee,
-      })
-      truffleAssert.eventEmitted(tx, 'LogOracleRegistered')
+      });
+      truffleAssert.eventEmitted(tx, "LogOracleRegistered");
 
       for (let i = 11; i <= 35; i++) {
-        await consortium.registerOracle({
+        tx = await oracle.registerOracle({
           from: accounts[i],
           value: fee,
-        })
+        });
+        truffleAssert.eventEmitted(tx, "LogOracleRegistered");
       }
-    })
+    });
 
-    it(`lets submit oracle response`, async () => {})
-    it(``, async () => {})
-  })
-})
+    it(`lets submit oracle response`, async () => {});
+  });
+});
