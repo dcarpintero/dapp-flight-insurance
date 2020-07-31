29efb5c564a166b072455a22e96e6eb917bdd08e
dcarpintero <6709785+dcarpintero@users.noreply.github.com>
2020-07-31 22:20:20 +0200

    Update TestOracle.js

--- test/TestOracle.js
+++ test/TestOracle.js
@@ -7,6 +7,13 @@ const truffleAssert = require("truffle-assertions");
 contract("Oracle", async (accounts) => {
   const ORACLE_FEE = web3.utils.toWei("1", "ether");
 
+  wrightBrothers = accounts[1];
+  KittyHawk = accounts[2];
+
+  passenger_1 = accounts[3];
+
+  flight_1 = web3.utils.utf8ToHex("WB1111"); // LATE_AIRLINE
+
   before("setup contract", async () => {
     insuranceHandler = await FlightInsuranceHandler.deployed();
     oracle = await ConsortiumOracle.deployed();
@@ -31,6 +38,17 @@ contract("Oracle", async (accounts) => {
       }
     });
 
-    it(`lets submit oracle response`, async () => {});
+    it(`lets submit oracle response`, async () => {
+      oracle.submitOracleResponse();
+    });
+
+    /*
+    function submitOracleResponse(
+      uint8 index,
+      address airline,
+      bytes32 flight,
+      uint256 timestamp,
+      uint8 statusCode
+  )*/
   });
 });
