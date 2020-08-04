c21a1506def37e2e06c675b44ffe08f3796ec97e
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2020-08-04 12:07:15 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,26 +1,26 @@
-import ConsortiumAlliance from "../../build/contracts/ConsortiumAlliance.json";
-import FlightInsuranceHandler from "../../build/contracts/flightInsuranceHandler.json";
+import ConsortiumAlliance from '../../build/contracts/ConsortiumAlliance.json'
+import FlightInsuranceHandler from '../../build/contracts/flightInsuranceHandler.json'
 
-import Config from "./config.json";
-import Web3 from "web3";
-import express from "express";
-import regeneratorRuntime from "regenerator-runtime";
+import Config from './config.json'
+import Web3 from 'web3'
+import express from 'express'
+import regeneratorRuntime from 'regenerator-runtime'
 
-let config = Config["localhost"];
+let config = Config['localhost']
 let web3 = new Web3(
-  new Web3.providers.WebsocketProvider(config.url.replace("http", "ws"))
-);
-web3.eth.defaultAccount = web3.eth.accounts[0];
+  new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')),
+)
+web3.eth.defaultAccount = web3.eth.accounts[0]
 
 let consortium = new web3.eth.Contract(
   ConsortiumAlliance.abi,
-  config.dataAddress
-);
+  config.dataAddress,
+)
 
 let insuranceHandler = new web3.eth.Contract(
   FlightInsuranceHandler.abi,
-  config.appAddress
-);
+  config.appAddress,
+)
 
 const Backend = {
   airlines: [],
@@ -28,37 +28,95 @@ const Backend = {
   oracles: [],
 
   initAirlines: async function () {
-    let accounts = await web3.eth.getAccounts();
-    let admin = accounts[0];
-    let wrightBrothers = accounts[1];
-    let kittyHawk = accounts[2];
-    const fee = web3.utils.toWei("10", "ether");
+    let accounts = await web3.eth.getAccounts()
+    let admin = accounts[0]
+    let wrightBrothers = accounts[1]
+    let kittyHawk = accounts[2]
+    const fee = web3.utils.toWei('10', 'ether')
 
-    console.log("Registering Airlines...");
-
-    await consortium.methods
-      .createAffiliate(wrightBrothers, "Wright Brothers")
-      .send({ from: admin, gas: 500000 });
+    console.log('Registering Airlines...')
 
     await consortium.methods
-      .createAffiliate(kittyHawk, "Kitty Hawk")
-      .send({ from: admin, gas: 500000 });
+      .createAffiliate(wrightBrothers, 'Wright Brothers')
+      .send({ from: admin, gas: 500000 })
+
+    await consortium.methods
+      .createAffiliate(kittyHawk, 'Kitty Hawk')
+      .send({ from: admin, gas: 500000 })
 
     await consortium.methods.depositMebership().send({
       from: wrightBrothers,
       value: fee,
       gas: 500000,
-    });
-    this.airlines.push({ title: "Wright Brothers", address: wrightBrothers });
+    })
+    this.airlines.push({ title: 'Wright Brothers', address: wrightBrothers })
+    console.log('\tWright Brothers has been registered')
 
     await consortium.methods.depositMebership().send({
       from: kittyHawk,
       value: fee,
       gas: 500000,
-    });
-    this.airlines.push({ title: "Kitty Hawk", address: kittyHawk });
+    })
+    this.airlines.push({ title: 'Kitty Hawk', address: kittyHawk })
+    console.log('\tKitty Hawk has been registered')
   },
-};
+
+  initFlights: async function () {
+    let accounts = await web3.eth.getAccounts()
+    let admin = accounts[0]
+    let wrightBrothers = accounts[1]
+    let kittyHawk = accounts[2]
+
+    var flight_1 = web3.utils.utf8ToHex('WB1111')
+    var flight_2 = web3.utils.utf8ToHex('WB2222')
+    var flight_3 = web3.utils.utf8ToHex('KH3333')
+    var flight_4 = web3.utils.utf8ToHex('KH4444')
+    var timestamp = 1111
+
+    console.log('Registering Flights...')
+    await insuranceHandler.methods.registerFlight(flight_1, timestamp).send({
+      from: wrightBrothers,
+    })
+    this.flights.push({
+      key: flight_1,
+      code: 'WB1111',
+      airline: wrightBrothers,
+      timestamp: timestamp,
+    })
+
+    await insuranceHandler.methods.registerFlight(flight_2, timestamp).send({
+      from: wrightBrothers,
+    })
+    this.flights.push({
+      key: flight_2,
+      code: 'WB2222',
+      airline: wrightBrothers,
+      timestamp: timestamp,
+    })
+
+    await insuranceHandler.methods.registerFlight(flight_3, timestamp).send({
+      from: kittyHawk,
+    })
+    this.flights.push({
+      key: flight_3,
+      code: 'KH3333',
+      airline: kittyHawk,
+      timestamp: timestamp,
+    })
+
+    await insuranceHandler.methods.registerFlight(flight_4, timestamp).send({
+      from: kittyHawk,
+    })
+    this.flights.push({
+      key: flight_4,
+      code: 'KH4444',
+      airline: kittyHawk,
+      timestamp: timestamp,
+    })
+
+    console.log('All flights have been registered')
+  },
+}
 
 /*
 flightSuretyApp.events.OracleRequest(
@@ -71,32 +129,35 @@ flightSuretyApp.events.OracleRequest(
   }
 );*/
 
-Backend.initAirlines();
+Backend.initAirlines()
+Backend.initFlights()
 
-const app = express();
+const app = express()
 
-app.get("/api", (req, res) => {
+app.get('/api', (req, res) => {
   res.send({
-    message: "v0.1.0",
-  });
-});
+    version: '0.1.0',
+  })
+})
 
-app.get("/airlines", (req, res) => {
-  res.send(Backend.airlines);
-});
+app.get('/airlines', (req, res) => {
+  res.send(Backend.airlines)
+})
 
-app.get("/airline/:address", async (req, res) => {
-  var address = req.params.address;
+app.get('/flights', (req, res) => {
+  res.send(Backend.flights)
+})
+
+app.get('/airline/:address', async (req, res) => {
+  var address = req.params.address
 
   consortium.methods.affiliates(address).call(function (err, result) {
     if (err) {
-      console.log(err.message);
+      console.log(err.message)
     } else {
-      console.log("call executed successfully.");
-      console.log(result);
-      res.send(result);
+      res.send(result)
     }
-  });
-});
+  })
+})
 
-export default app;
+export default app
