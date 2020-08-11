3e72e9708d15e46df1bbf25d89262691238a8d85
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2020-08-11 23:27:18 +0200

    Refactor

--- test/TestFlightInsuranceHandler.js
+++ test/TestFlightInsuranceHandler.js
@@ -1,29 +1,29 @@
-const ConsortiumAlliance = artifacts.require("ConsortiumAlliance");
-const FlightInsuranceHandler = artifacts.require("FlightInsuranceHandler");
-const assert = require("chai").assert;
-const truffleAssert = require("truffle-assertions");
+const ConsortiumAlliance = artifacts.require('ConsortiumAlliance')
+const FlightInsuranceHandler = artifacts.require('FlightInsuranceHandler')
+const assert = require('chai').assert
+const truffleAssert = require('truffle-assertions')
 
-contract("FlightInsuranceHandler", async (accounts) => {
-  const MEMBERSHIP_FEE = web3.utils.toWei("10", "ether");
-  const INSURANCE_FEE = web3.utils.toWei("1", "ether");
-  const ORACLE_FEE = web3.utils.toWei("1", "ether");
-  const TEST_ORACLES_COUNT = 20;
+contract('FlightInsuranceHandler', async (accounts) => {
+  const MEMBERSHIP_FEE = web3.utils.toWei('10', 'ether')
+  const INSURANCE_FEE = web3.utils.toWei('1', 'ether')
+  const ORACLE_FEE = web3.utils.toWei('1', 'ether')
+  const TEST_ORACLES_COUNT = 20
 
-  var flight_key_1;
-  var flight_key_2;
-  var flight_key_3;
-  var flight_key_4;
+  var flight_key_1
+  var flight_key_2
+  var flight_key_3
+  var flight_key_4
 
-  var insurance_F1_P1; // LATE_AIRLINE
-  var insurance_F2_P2; // LATE_AIRLINE
-  var insurance_F2_P3; // LATE_AIRLINE
-  var insurance_F3_P4; // ONTIME
-  var insurance_F4_P5; // LATE_OTHER
+  var insurance_F1_P1 // LATE_AIRLINE
+  var insurance_F2_P2 // LATE_AIRLINE
+  var insurance_F2_P3 // LATE_AIRLINE
+  var insurance_F3_P4 // ONTIME
+  var insurance_F4_P5 // LATE_OTHER
 
-  var request_key_F1;
-  var request_key_F2;
-  var request_key_F3;
-  var request_key_F4;
+  var request_key_F1
+  var request_key_F2
+  var request_key_F3
+  var request_key_F4
 
   const FlightStatus = {
     UNKNOWN: 0,
@@ -32,213 +32,193 @@ contract("FlightInsuranceHandler", async (accounts) => {
     LATE_WEATHER: 3,
     LATE_TECHNICAL: 4,
     LATE_OTHER: 5,
-  };
+  }
 
-  before("setup contract", async () => {
-    admin = accounts[0];
-    wrightBrothers = accounts[1];
-    KittyHawk = accounts[2];
+  before('setup contract', async () => {
+    admin = accounts[0]
+    wrightBrothers = accounts[1]
+    KittyHawk = accounts[2]
 
-    passenger_1 = accounts[3];
-    passenger_2 = accounts[4];
-    passenger_3 = accounts[5];
-    passenger_4 = accounts[6];
-    passenger_5 = accounts[7];
+    passenger_1 = accounts[3]
+    passenger_2 = accounts[4]
+    passenger_3 = accounts[5]
+    passenger_4 = accounts[6]
+    passenger_5 = accounts[7]
 
-    flight_1 = web3.utils.utf8ToHex("WB1111"); // LATE_AIRLINE
-    flight_2 = web3.utils.utf8ToHex("WB2222"); // LATE_AIRLINE
-    flight_3 = web3.utils.utf8ToHex("KH3333"); // ONTIME
-    flight_4 = web3.utils.utf8ToHex("KH4444"); // LATE_OTHER
+    flight_1 = web3.utils.utf8ToHex('WB1111') // LATE_AIRLINE
+    flight_2 = web3.utils.utf8ToHex('WB2222') // LATE_AIRLINE
+    flight_3 = web3.utils.utf8ToHex('KH3333') // ONTIME
+    flight_4 = web3.utils.utf8ToHex('KH4444') // LATE_OTHER
 
-    consortium = await ConsortiumAlliance.deployed();
-    insuranceHandler = await FlightInsuranceHandler.deployed();
-  });
+    consortium = await ConsortiumAlliance.deployed()
+    insuranceHandler = await FlightInsuranceHandler.deployed()
+  })
 
-  describe("Flight Insurance Workflow", function () {
+  describe('Flight Insurance Workflow', function () {
     it(`lets add Delegate`, async () => {
       await consortium.addDelegateRole(FlightInsuranceHandler.address, {
         from: admin,
-      });
+      })
 
-      assert.isTrue(
-        await consortium.isDelegate(FlightInsuranceHandler.address)
-      );
-    });
+      assert.isTrue(await consortium.isDelegate(FlightInsuranceHandler.address))
+    })
 
     it(`lets be operational after deployment`, async () => {
-      assert.isTrue(await consortium.isOperational());
-      assert.isTrue(await insuranceHandler.isOperational());
-    });
+      assert.isTrue(await consortium.isOperational())
+      assert.isTrue(await insuranceHandler.isOperational())
+    })
 
     it(`lets register first airline`, async () => {
-      tx = await consortium.createAffiliate(wrightBrothers, "Wright Brothers");
-      truffleAssert.eventEmitted(tx, "LogAffiliateRegistered");
-    });
+      tx = await consortium.createAffiliate(wrightBrothers, 'Wright Brothers')
+      truffleAssert.eventEmitted(tx, 'LogAffiliateRegistered')
+    })
 
     it(`lets register second airline`, async () => {
-      tx = await consortium.createAffiliate(KittyHawk, "Kitty Hawk");
-      truffleAssert.eventEmitted(tx, "LogAffiliateRegistered");
-    });
+      tx = await consortium.createAffiliate(KittyHawk, 'Kitty Hawk')
+      truffleAssert.eventEmitted(tx, 'LogAffiliateRegistered')
+    })
 
     it(`lets deposit consortium memberships`, async () => {
-      let fee = MEMBERSHIP_FEE;
+      let fee = MEMBERSHIP_FEE
 
       let tx_1 = await consortium.depositMebership({
         from: wrightBrothers,
         value: fee,
-      });
-      truffleAssert.eventEmitted(tx_1, "LogAffiliateFunded");
+      })
+      truffleAssert.eventEmitted(tx_1, 'LogAffiliateFunded')
 
       let tx_2 = await consortium.depositMebership({
         from: KittyHawk,
         value: fee,
-      });
-      truffleAssert.eventEmitted(tx_2, "LogAffiliateFunded");
-    });
+      })
+      truffleAssert.eventEmitted(tx_2, 'LogAffiliateFunded')
+    })
 
     it(`lets register Wright Brothers' flights`, async () => {
       let flight_tx_1 = await insuranceHandler.registerFlight(flight_1, 1111, {
         from: wrightBrothers,
-      });
-      truffleAssert.eventEmitted(flight_tx_1, "LogFlightRegistered");
-      flight_key_1 = flight_tx_1.logs[0].args["key"];
+      })
+      truffleAssert.eventEmitted(flight_tx_1, 'LogFlightRegistered')
+      flight_key_1 = flight_tx_1.logs[0].args['key']
 
       let flight_tx_2 = await insuranceHandler.registerFlight(flight_2, 2222, {
         from: wrightBrothers,
-      });
-      truffleAssert.eventEmitted(flight_tx_2, "LogFlightRegistered");
-      flight_key_2 = flight_tx_2.logs[0].args["key"];
-    });
+      })
+      truffleAssert.eventEmitted(flight_tx_2, 'LogFlightRegistered')
+      flight_key_2 = flight_tx_2.logs[0].args['key']
+    })
 
     it(`lets register Kitty Hawk' flights`, async () => {
       let flight_tx_3 = await insuranceHandler.registerFlight(flight_3, 3333, {
         from: KittyHawk,
-      });
-      truffleAssert.eventEmitted(flight_tx_3, "LogFlightRegistered");
-      flight_key_3 = flight_tx_3.logs[0].args["key"];
+      })
+      truffleAssert.eventEmitted(flight_tx_3, 'LogFlightRegistered')
+      flight_key_3 = flight_tx_3.logs[0].args['key']
 
       let flight_tx_4 = await insuranceHandler.registerFlight(flight_4, 4444, {
         from: KittyHawk,
-      });
-      truffleAssert.eventEmitted(flight_tx_4, "LogFlightRegistered");
-      flight_key_4 = flight_tx_4.logs[0].args["key"];
-    });
+      })
+      truffleAssert.eventEmitted(flight_tx_4, 'LogFlightRegistered')
+      flight_key_4 = flight_tx_4.logs[0].args['key']
+    })
 
     it(`lets create flight insurances`, async () => {
       let tx_1 = await insuranceHandler.registerFlightInsurance(flight_key_1, {
         from: passenger_1,
         value: INSURANCE_FEE,
-      });
+      })
 
       let tx_2 = await insuranceHandler.registerFlightInsurance(flight_key_2, {
         from: passenger_2,
         value: INSURANCE_FEE,
-      });
+      })
 
       let tx_3 = await insuranceHandler.registerFlightInsurance(flight_key_2, {
         from: passenger_3,
         value: INSURANCE_FEE,
-      });
+      })
 
       let tx_4 = await insuranceHandler.registerFlightInsurance(flight_key_3, {
         from: passenger_4,
         value: INSURANCE_FEE,
-      });
+      })
 
       let tx_5 = await insuranceHandler.registerFlightInsurance(flight_key_4, {
         from: passenger_5,
         value: INSURANCE_FEE,
-      });
+      })
 
-      insurance_F1_P1 = tx_1.logs[0].args["key"];
-      insurance_F2_P2 = tx_2.logs[0].args["key"];
-      insurance_F2_P3 = tx_3.logs[0].args["key"];
-      insurance_F3_P4 = tx_4.logs[0].args["key"];
-      insurance_F4_P5 = tx_5.logs[0].args["key"];
+      insurance_F1_P1 = tx_1.logs[0].args['key']
+      insurance_F2_P2 = tx_2.logs[0].args['key']
+      insurance_F2_P3 = tx_3.logs[0].args['key']
+      insurance_F3_P4 = tx_4.logs[0].args['key']
+      insurance_F4_P5 = tx_5.logs[0].args['key']
 
-      truffleAssert.eventEmitted(tx_1, "LogFlightInsuranceRegistered");
-      truffleAssert.eventEmitted(tx_2, "LogFlightInsuranceRegistered");
-      truffleAssert.eventEmitted(tx_3, "LogFlightInsuranceRegistered");
-      truffleAssert.eventEmitted(tx_4, "LogFlightInsuranceRegistered");
-      truffleAssert.eventEmitted(tx_5, "LogFlightInsuranceRegistered");
-    });
+      truffleAssert.eventEmitted(tx_1, 'LogFlightInsuranceRegistered')
+      truffleAssert.eventEmitted(tx_2, 'LogFlightInsuranceRegistered')
+      truffleAssert.eventEmitted(tx_3, 'LogFlightInsuranceRegistered')
+      truffleAssert.eventEmitted(tx_4, 'LogFlightInsuranceRegistered')
+      truffleAssert.eventEmitted(tx_5, 'LogFlightInsuranceRegistered')
+    })
 
     it(`lets request flight status`, async () => {
-      let tx_1 = await insuranceHandler.requestFlightStatus(
-        wrightBrothers,
-        flight_1,
-        1111
-      );
-      truffleAssert.eventEmitted(tx_1, "LogFlightStatusRequested");
-      request_key_F1 = tx_1.logs[0].args["key"];
+      let tx_1 = await insuranceHandler.requestFlightStatus(flight_key_1)
+      truffleAssert.eventEmitted(tx_1, 'LogFlightStatusRequested')
+      request_key_F1 = tx_1.logs[0].args['key']
 
-      let tx_2 = await insuranceHandler.requestFlightStatus(
-        wrightBrothers,
-        flight_2,
-        2222
-      );
-      truffleAssert.eventEmitted(tx_2, "LogFlightStatusRequested");
-      request_key_F2 = tx_2.logs[0].args["key"];
+      let tx_2 = await insuranceHandler.requestFlightStatus(flight_key_2)
+      truffleAssert.eventEmitted(tx_2, 'LogFlightStatusRequested')
+      request_key_F2 = tx_2.logs[0].args['key']
 
-      let tx_3 = await insuranceHandler.requestFlightStatus(
-        KittyHawk,
-        flight_3,
-        3333
-      );
-      truffleAssert.eventEmitted(tx_3, "LogFlightStatusRequested");
-      request_key_F3 = tx_3.logs[0].args["key"];
-    });
+      let tx_3 = await insuranceHandler.requestFlightStatus(flight_key_3)
+      truffleAssert.eventEmitted(tx_3, 'LogFlightStatusRequested')
+      request_key_F3 = tx_3.logs[0].args['key']
+    })
 
     // _processFlightStatus is an internal function - this workflow is now in TestOracle.js
     xit(`lets process flight status - LATE_AIRLINE - 1 Passenger`, async () => {
-      let status_F1 = FlightStatus.LATE_AIRLINE;
+      let status_F1 = FlightStatus.LATE_AIRLINE
 
       let process_tx_1 = await insuranceHandler._processFlightStatus(
         request_key_F1,
-        wrightBrothers,
-        flight_1,
-        1111,
+        flight_key_1,
         status_F1,
         {
           from: admin,
-        }
-      );
-      truffleAssert.eventEmitted(process_tx_1, "LogFlightStatusProcessed");
-      truffleAssert.eventEmitted(process_tx_1, "LogInsureeCredited");
-    });
+        },
+      )
+      truffleAssert.eventEmitted(process_tx_1, 'LogFlightStatusProcessed')
+      truffleAssert.eventEmitted(process_tx_1, 'LogInsureeCredited')
+    })
 
     xit(`lets process flight status - LATE_AIRLINE - 2 Passengers`, async () => {
-      let status_F2 = FlightStatus.LATE_AIRLINE;
+      let status_F2 = FlightStatus.LATE_AIRLINE
 
       let process_tx_2 = await insuranceHandler._processFlightStatus(
         request_key_F2,
-        wrightBrothers,
-        flight_2,
-        2222,
+        flight_key_2,
         status_F2,
         {
           from: admin,
-        }
-      );
-      truffleAssert.eventEmitted(process_tx_2, "LogFlightStatusProcessed");
-      truffleAssert.eventEmitted(process_tx_2, "LogInsureeCredited");
-    });
+        },
+      )
+      truffleAssert.eventEmitted(process_tx_2, 'LogFlightStatusProcessed')
+      truffleAssert.eventEmitted(process_tx_2, 'LogInsureeCredited')
+    })
 
     xit(`lets process flight status - ON_TIME`, async () => {
-      let status_F3 = FlightStatus.ON_TIME;
+      let status_F3 = FlightStatus.ON_TIME
 
       let process_tx_3 = await insuranceHandler._processFlightStatus(
         request_key_F3,
-        KittyHawk,
-        flight_3,
-        3333,
+        flight_key_3,
         status_F3,
         {
           from: admin,
-        }
-      );
-      truffleAssert.eventEmitted(process_tx_3, "LogFlightStatusProcessed");
-      truffleAssert.eventEmitted(process_tx_3, "LogConsortiumCredited");
-    });
-  });
-});
+        },
+      )
+      truffleAssert.eventEmitted(process_tx_3, 'LogFlightStatusProcessed')
+      truffleAssert.eventEmitted(process_tx_3, 'LogConsortiumCredited')
+    })
+  })
+})
--- test/TestOraclesAndPayments.js
+++ test/TestOraclesAndPayments.js
@@ -1,16 +1,16 @@
-const ConsortiumAlliance = artifacts.require("ConsortiumAlliance");
-const FlightInsuranceHandler = artifacts.require("FlightInsuranceHandler");
-const assert = require("chai").assert;
-const truffleAssert = require("truffle-assertions");
+const ConsortiumAlliance = artifacts.require('ConsortiumAlliance')
+const FlightInsuranceHandler = artifacts.require('FlightInsuranceHandler')
+const assert = require('chai').assert
+const truffleAssert = require('truffle-assertions')
 
-contract("FlightInsuranceHandler", async (accounts) => {
-  const MEMBERSHIP_FEE = web3.utils.toWei("10", "ether");
-  const INSURANCE_FEE = web3.utils.toWei("1", "ether");
-  const ORACLE_FEE = web3.utils.toWei("1", "ether");
-  const TEST_ORACLES_COUNT = 25;
-  const ORACLE_CONSENSUS = 3;
+contract('FlightInsuranceHandler', async (accounts) => {
+  const MEMBERSHIP_FEE = web3.utils.toWei('10', 'ether')
+  const INSURANCE_FEE = web3.utils.toWei('1', 'ether')
+  const ORACLE_FEE = web3.utils.toWei('1', 'ether')
+  const TEST_ORACLES_COUNT = 25
+  const ORACLE_CONSENSUS = 3
 
-  var flight_key_1;
+  var flight_key_1
 
   const FlightStatus = {
     UNKNOWN: 0,
@@ -19,67 +19,65 @@ contract("FlightInsuranceHandler", async (accounts) => {
     LATE_WEATHER: 3,
     LATE_TECHNICAL: 4,
     LATE_OTHER: 5,
-  };
+  }
 
-  before("setup contract", async () => {
-    admin = accounts[0];
+  before('setup contract', async () => {
+    admin = accounts[0]
 
-    wrightBrothers = accounts[31];
-    KittyHawk = accounts[32];
+    wrightBrothers = accounts[31]
+    KittyHawk = accounts[32]
 
-    passenger_1 = accounts[33];
-    passenger_2 = accounts[34];
+    passenger_1 = accounts[33]
+    passenger_2 = accounts[34]
 
-    flight_1 = web3.utils.utf8ToHex("WB1111");
-    timestamp_flight_1 = Math.floor(Date.now() / 1000);
+    flight_1 = web3.utils.utf8ToHex('WB1111')
+    timestamp_flight_1 = Math.floor(Date.now() / 1000)
 
-    consortium = await ConsortiumAlliance.deployed();
-    insuranceHandler = await FlightInsuranceHandler.deployed();
-  });
+    consortium = await ConsortiumAlliance.deployed()
+    insuranceHandler = await FlightInsuranceHandler.deployed()
+  })
 
-  describe("Oracle Registration and Responses", function () {
+  describe('Oracle Registration and Responses', function () {
     it(`lets add Delegate`, async () => {
       await consortium.addDelegateRole(FlightInsuranceHandler.address, {
         from: admin,
-      });
+      })
 
-      assert.isTrue(
-        await consortium.isDelegate(FlightInsuranceHandler.address)
-      );
-    });
+      assert.isTrue(await consortium.isDelegate(FlightInsuranceHandler.address))
+    })
 
     it(`lets register oracles`, async () => {
-      let fee = ORACLE_FEE;
+      let fee = ORACLE_FEE
 
       let tx = await insuranceHandler.registerOracle({
         from: accounts[10],
         value: fee,
-      });
-      truffleAssert.eventEmitted(tx, "LogOracleRegistered");
+      })
+      truffleAssert.eventEmitted(tx, 'LogOracleRegistered')
 
       for (let i = 1; i <= TEST_ORACLES_COUNT; i++) {
         tx = await insuranceHandler.registerOracle({
           from: accounts[i],
           value: fee,
-        });
-        truffleAssert.eventEmitted(tx, "LogOracleRegistered");
+        })
+        truffleAssert.eventEmitted(tx, 'LogOracleRegistered')
         let oracleIndexes = await insuranceHandler.getMyIndexes.call({
           from: accounts[i],
-        });
+        })
         console.log(
-          `\t oracle Registered: ${oracleIndexes[0]}, ${oracleIndexes[1]}, ${oracleIndexes[2]}`
-        );
+          `\t oracle Registered: ${oracleIndexes[0]}, ${oracleIndexes[1]}, ${oracleIndexes[2]}`,
+        )
       }
-    });
+    })
 
     it(`lets register airline`, async () => {
-      await consortium.createAffiliate(wrightBrothers, "Wright Brothers");
+      await consortium.createAffiliate(wrightBrothers, 'Wright Brothers')
 
       await consortium.depositMebership({
         from: wrightBrothers,
         value: MEMBERSHIP_FEE,
-      });
-    });
+      })
+    })
 
     it(`lets register Wright Brothers' flight`, async () => {
       let flight_tx_1 = await insuranceHandler.registerFlight(
@@ -87,120 +85,113 @@ contract("FlightInsuranceHandler", async (accounts) => {
         timestamp_flight_1,
         {
           from: wrightBrothers,
-        }
-      );
+        },
+      )
 
-      flight_key_1 = flight_tx_1.logs[0].args["key"];
-    });
+      flight_key_1 = flight_tx_1.logs[0].args['key']
+    })
 
     it(`lets create flight insurances`, async () => {
       let tx_1 = await insuranceHandler.registerFlightInsurance(flight_key_1, {
         from: passenger_1,
         value: INSURANCE_FEE,
-      });
+      })
 
       let tx_2 = await insuranceHandler.registerFlightInsurance(flight_key_1, {
         from: passenger_2,
         value: INSURANCE_FEE,
-      });
+      })
 
-      insurance_F1_P1 = tx_1.logs[0].args["key"];
-      insurance_F2_P2 = tx_2.logs[0].args["key"];
+      insurance_F1_P1 = tx_1.logs[0].args['key']
+      insurance_F2_P2 = tx_2.logs[0].args['key']
 
-      truffleAssert.eventEmitted(tx_1, "LogFlightInsuranceRegistered");
-      truffleAssert.eventEmitted(tx_2, "LogFlightInsuranceRegistered");
-    });
+      truffleAssert.eventEmitted(tx_1, 'LogFlightInsuranceRegistered')
+      truffleAssert.eventEmitted(tx_2, 'LogFlightInsuranceRegistered')
+    })
 
     it(`lets submit oracle responses and trigger process flight`, async () => {
-      let req_tx = await insuranceHandler.requestFlightStatus(
-        wrightBrothers,
-        flight_1,
-        timestamp_flight_1,
-        {
-          from: admin,
-          nonce: await web3.eth.getTransactionCount(admin),
-        }
-      );
-      truffleAssert.eventEmitted(req_tx, "LogFlightStatusRequested");
-      let index = req_tx.logs[0].args["index"];
-      let totalResponses = 0;
-      console.log("\t valid index: " + index);
+      let req_tx = await insuranceHandler.requestFlightStatus(flight_key_1, {
+        from: admin,
+        nonce: await web3.eth.getTransactionCount(admin),
+      })
+      truffleAssert.eventEmitted(req_tx, 'LogFlightStatusRequested')
+      let index = req_tx.logs[0].args['index']
+      let totalResponses = 0
+      console.log('\t valid index: ' + index)
 
       for (let i = 1; i <= TEST_ORACLES_COUNT; i++) {
         let oracleIndexes = await insuranceHandler.getMyIndexes.call({
           from: accounts[i],
-        });
+        })
 
         for (let idx = 0; idx < 3; idx++) {
           if (index == oracleIndexes[idx].toNumber()) {
             console.log(
-              "\t submitting oracle response: " + oracleIndexes[idx].toNumber()
-            );
+              '\t submitting oracle response: ' + oracleIndexes[idx].toNumber(),
+            )
             let status_tx = await insuranceHandler.submitOracleResponse(
               oracleIndexes[idx],
-              wrightBrothers,
-              flight_1,
-              timestamp_flight_1,
+              flight_key_1,
               FlightStatus.LATE_AIRLINE,
               {
                 from: accounts[i],
                 nonce: await web3.eth.getTransactionCount(accounts[i]),
-              }
-            );
-            truffleAssert.eventEmitted(status_tx, "LogOracleReport");
-            totalResponses = totalResponses + 1;
+              },
+            )
+            truffleAssert.eventEmitted(status_tx, 'LogOracleReport')
+            totalResponses = totalResponses + 1
 
             if (totalResponses == ORACLE_CONSENSUS) {
-              console.log("\t reached Oracle consensus");
-              truffleAssert.eventEmitted(status_tx, "LogFlightStatus");
-              truffleAssert.eventEmitted(status_tx, "LogFlightStatusProcessed");
-              return;
+              console.log('\t reached Oracle consensus')
+              truffleAssert.eventEmitted(status_tx, 'LogFlightStatusResolved')
+              truffleAssert.eventEmitted(status_tx, 'LogFlightStatusProcessed')
+              return
             }
           }
         }
       }
-    });
+    })
 
     it(`lets credit escrow account with insruance premium`, async () => {
-      const deposit = INSURANCE_FEE;
-      const premium_factor = 50;
-      const premium = web3.utils.toBN(deposit * (premium_factor / 100));
-      const withdraw_amount = web3.utils.toBN(deposit).add(premium);
+      const deposit = INSURANCE_FEE
+      const premium_factor = 50
+      const premium = web3.utils.toBN(deposit * (premium_factor / 100))
+      const withdraw_amount = web3.utils.toBN(deposit).add(premium)
 
       assert.equal(
         Number(await consortium.payments(passenger_1)),
-        withdraw_amount
-      );
+        withdraw_amount,
+      )
 
       assert.equal(
         Number(await consortium.payments(passenger_2)),
-        withdraw_amount
-      );
-    });
+        withdraw_amount,
+      )
+    })
 
     it(`lets withdraw funds by insurees`, async () => {
-      const deposit = INSURANCE_FEE;
-      const premium_factor = 50;
-      const premium = web3.utils.toBN(deposit * (premium_factor / 100));
-      const withdraw_amount = web3.utils.toBN(deposit).add(premium);
+      const deposit = INSURANCE_FEE
+      const premium_factor = 50
+      const premium = web3.utils.toBN(deposit * (premium_factor / 100))
+      const withdraw_amount = web3.utils.toBN(deposit).add(premium)
 
-      const payeeBalanceBefore = await web3.eth.getBalance(passenger_1);
+      const payeeBalanceBefore = await web3.eth.getBalance(passenger_1)
       let wdrw_tx = await consortium.withdrawPayments(passenger_1, {
         from: passenger_1,
-      });
-      const payeeBalanceAfter = await web3.eth.getBalance(passenger_1);
-      const tx = await web3.eth.getTransaction(wdrw_tx.tx);
+      })
+      const payeeBalanceAfter = await web3.eth.getBalance(passenger_1)
+      const tx = await web3.eth.getTransaction(wdrw_tx.tx)
 
-      let gasPrice = tx.gasPrice;
-      let gasUsed = wdrw_tx.receipt.gasUsed;
-      const gas = web3.utils.toBN(gasPrice).mul(web3.utils.toBN(gasUsed));
+      let gasPrice = tx.gasPrice
+      let gasUsed = wdrw_tx.receipt.gasUsed
+      const gas = web3.utils.toBN(gasPrice).mul(web3.utils.toBN(gasUsed))
 
       assert.equal(
         web3.utils
           .toBN(payeeBalanceAfter)
           .sub(web3.utils.toBN(payeeBalanceBefore)),
-        Number(withdraw_amount) - Number(gas)
-      );
-    });
-  });
-});
+        Number(withdraw_amount) - Number(gas),
+      )
+    })
+  })
+})
